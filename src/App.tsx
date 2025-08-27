ed72bb338d33b9a930364a24659f27ec72d27ee2
JerryHelmets <rssteilen@gmail.com>
2025-08-27 14:58:49 -0400

    Refactor App component to simplify routing
    Removed BrowserRouter and Navigate components, wrapping Routes in a div instead.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// src/App.tsx
+import { Routes, Route } from "react-router-dom";
 import GameComponent from "./GameComponent";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Routes>
         <Route path="/" element={<GameComponent />} />
-        {/* Route away anything else to the game */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
