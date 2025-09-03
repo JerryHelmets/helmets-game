123d3dd5255a3e1bf9e90598a174cee820a8013e
JerryHelmets <rssteilen@gmail.com>
2025-09-03 14:49:49 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")!
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      {/* Vercel Web Analytics */}
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
