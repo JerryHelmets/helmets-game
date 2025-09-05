2d981d67443ed128e2aff3d1072a6730f834bd07
JerryHelmets <rssteilen@gmail.com>
2025-09-05 06:40:35 -0400

    add admin console

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // src/App.tsx
 import { Routes, Route } from "react-router-dom";
 import GameComponent from "./GameComponent";
+import AdminConsole from "./AdminConsole";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<GameComponent />} />
+        <Route path="/admin" element={<AdminConsole />} />
       </Routes>
     </div>
   );
